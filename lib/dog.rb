9efeef80cda2bfc6c28ea4c1c3d98c4fadd9fb5f
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-11 19:32:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Dog
   def name=(dog_name)
     @this_dogs_name =dog_name
@@ -22,3 +23,24 @@ class Dog
     puts "woof!"
   end 
 end 
+=======
+# Your code goes here!
+class Dog 
+
+end
+
+
+class Dog
+  def name=(fido)
+    dogs_name = Fido
+  end
+ 
+  
+
+class Dog
+ 
+  def bark
+    puts "woof!"
+  end
+end
+>>>>>>> 6a5901ba2b360d0a901d347f564158e992e3c91d
